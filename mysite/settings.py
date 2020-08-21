5d78ba7f87a694124d3c874334f89cc6654e4e47
kevin <kiariekevin22@gmail.com>
2020-08-21 11:31:32 +0300

    gitignore, settings and requirements files

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,5 +124,6 @@ STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, "static"),
                    '/var/www/static',
                    ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
