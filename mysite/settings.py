fbbd75561d9a31db72e3bebb83354c7dc764bb0f
kevin <kiariekevin22@gmail.com>
2020-08-21 12:06:35 +0300

    modified allowed hosts in seeings fie

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'sbd@gr5pc0h275vwkeibka7qprhddcy=9_8szqo(=fi+(eol&!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['kiariesite.herokuapp.com']
 
 
 # Application definition
