b534d1fc2ac108cf7df66c24214304d64d8d3fab
kevin <kiariekevin22@gmail.com>
2020-08-26 09:38:15 +0300

    logout template fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 LOGIN_REDIRECT_URL='/todo/tasks'
-LOGOUT_REDIRECT_URL='/users/logout/'
 STATIC_URL = '/static/'
 STATICFILES_DIR = [os.path.join(BASE_DIR, "static"),
                    '/var/www/static',
